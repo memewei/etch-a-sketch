29f9cfd3436fdd4d39fc2859e47a34db49e740a7
memewei <minwei2002@gmail.com>
2023-07-21 15:07:11 +0800

    Add change grid size function

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 What I learnt from this project:
 21-Jul-2023
 - CSS gridTemplateColumn & gridTemplateRow
+
+to do:
+- add the option for them to click instead of hovering
+- add prompt for users to change grid size
+- randomize the colors if the user chooses rainbow mode
+- "shading" mouseover
+- returning multiple values in functions
--- script.js
+++ script.js
@@ -1,23 +1,62 @@
 var grid = document.getElementById('grid');
-const size = 16;
+const defaultSize = 16;
 const currentColor = '#333333';
+const toggleBtn = document.querySelector('.toggle');
+const checkBox = document.querySelector('#checkbox');
+const clearGridBtn = document.querySelector("#clear");
+const newSize = document.querySelector('#new-size');
+var mode1 = 'mouseenter';
+var mode2 = '';
+var switchMode = true;
+let currentSize = defaultSize;
 
 
-function buildGrid(size){
-    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+toggleBtn.addEventListener('click', () => {
+    toggleBtn.classList.toggle("active");
+    mode1 = 'mousedown';
+    mode2 = 'mouseover';
+    return mode1;
+});
+clearGridBtn.addEventListener('click', () => refreshGrid());
+newSize.addEventListener('click', () => {
+    setNewSize();
+    refreshGrid();
+});
 
-    for(i=0; i < size * size; i++){
+
+console.log(mode1);
+console.log(mode2);
+function buildGrid(e){
+    grid.style.gridTemplateColumns = `repeat(${currentSize}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${currentSize}, 1fr)`;
+
+    for(i=0; i < currentSize * currentSize; i++){
         const gridDiv = document.createElement('div');
         gridDiv.classList.add('grid-div');
-        gridDiv.addEventListener('mouseenter', changeColor);
+        gridDiv.addEventListener(mode1, changeColor);
+        gridDiv.addEventListener(mode2, changeColor);
         grid.appendChild(gridDiv);
-        console.log("ok");
     }
+}
+
+function setNewSize(value){
+    currentSize = prompt("What size will your new grid be? Maximum: 100");
+    return currentSize;
+}
+
+function clearGrid(){
+    grid.innerHTML = " ";
+}
+
+function refreshGrid(){
+    clearGrid();
+    buildGrid(currentSize);
 }
 
 function changeColor(e){
     if(e.type === 'mouseenter'){
+        e.target.style.backgroundColor = currentColor;
+    }else if(e.type === 'mousedown'){
         e.target.style.backgroundColor = currentColor;
     }else{
         return;
@@ -25,5 +64,5 @@ function changeColor(e){
 }
 
 window.onload = () =>{
-    buildGrid(size);
+    buildGrid(currentSize);
 }
